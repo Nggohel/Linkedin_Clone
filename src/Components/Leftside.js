246861687523bc3dd61d288a7225ff68d8810eee
Nggohel <gohelnidhi15898@gmail.com>
2023-08-28 10:44:12 +0530

    build created

--- src/Components/Leftside.js
+++ src/Components/Leftside.js
@@ -8,3 +8,48 @@ const Container = styled.div`
   grid-area: leftside;
 `;
 export default Leftside;
+// display: flex;
+//   align-items: start;
+//   min-height: 700px;
+//   padding-bottom: 138px;
+//   padding-top: 40px;
+//   padding: 60px 0;
+//   position: relative;
+//   flex-wrap: wrap;
+//   width: 100%;
+//   max-width: 1128px;
+//   align-items: center;
+//   margin: auto;
+//   @media (max-width: 798);
+//   min-height: 0px;
+//   margin: auto;
+//   width: 100%;
+//   h1 {
+//     padding-bottom: 0;
+//     width: 55%;
+//     font-size: 56px;
+//     color: #2977c9;
+//     line-height: 70px;
+
+//     @media (max-width: 768px) {
+//       align-items: center;
+//       font-size: 20px;
+//       width: 100%;
+//       line-height: 2;
+//     }
+//     box-shadow: inset 0 0 0 1px #0a66c2;
+//   color: #0a66c2;
+//   border-radius: 24px;
+//   transition-duration: 167ms;
+//   font-size: 16px;
+//   font-weight: 600;
+//   line-height: 40px;
+//   padding: 10px 24px;
+//   text-align: center;
+//   background-color: rgba(0, 0, 0, 0);
+//   &:hover {
+//     background-color: rgba(112, 181, 249, 0.15);
+//     color: #0a66c2;
+//     text-decoration: none;
+//     border-radius: 4px;
+//   }
