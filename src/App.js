c02fb70dcee09cd1b0fc59935adde06749a68165
Nggohel <gohelnidhi15898@gmail.com>
2023-08-28 09:22:18 +0530

    nav bar created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Outlet,
+} from "react-router-dom";
+import "./App.css";
+import Login from "./Components/Login";
+import Home from "./Components/Home";
+import Header from "./Components/Header";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Login />}></Route>
+          <Route exact path="/home" element={<HomeLayout />}></Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
 
+function HomeLayout() {
+  return (
+    <>
+      <Outlet />
+      <Home></Home>
+      <Header />
+    </>
+  );
+}
 export default App;
